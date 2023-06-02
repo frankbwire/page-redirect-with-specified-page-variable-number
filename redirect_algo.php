14acbac181d63b95178f0043c20278dc35b2c540
frank_bwire <bwire450@gmail.com>
2023-06-02 21:59:04 +0300

    Add files via upload

---
+++ redirect_algo.php
@@ -0,0 +1,12 @@
+<!-- The code you provided  sets the $branch_id variable to 5, and then displays an alert message "Error Deleting Extension". After that, it redirects the page to '../branches.php?branch_id=' . $branch_id using JavaScript's window.location property.
+
+The URL in the window.location will include the branch_id query parameter with the value of the $branch_id variable, allowing you to pass the ID to the branches.php page. Make sure that the relative path '../branches.php' is correct and corresponds to the actual location of your branches.php file. -->
+
+<?php
+$branch_id=5;
+
+echo "<script type='text/javascript'>alert('Error Deleting Extension');
+        window.location='../branches.php?branch_id=" . $branch_id . "';
+        </script>";
+
+?>
